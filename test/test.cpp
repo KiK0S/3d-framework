d0567fb219c7b6e0d236f52f32fe4914451c2bac
Konstantin Amelichev <kostya.amelichev@gmail.com>
2021-06-04 02:30:55 +0300

    black cube for tag v0.2

--- test/test.cpp
+++ test/test.cpp
@@ -11,14 +11,7 @@
 
 int main() {
     app::Application application;
-    application.add_object(std::make_unique<app::Cube>(app::Point4d{0, 0, 100}, sf::Color::Red));
-    application.add_object(std::make_unique<app::Cube>(app::Point4d{0, 100, 100}, sf::Color::Yellow));
-    application.add_object(std::make_unique<app::Cube>(app::Point4d{100, 100, 100}, sf::Color::White));
-    application.add_object(std::make_unique<app::Cube>(app::Point4d{100, 0, 100}, sf::Color::Green));
-    application.add_object(std::make_unique<app::Cube>(app::Point4d{0, 0, 201}, sf::Color::Blue));
-    application.add_object(std::make_unique<app::Cube>(app::Point4d{0, 100, 201}, sf::Color::Black));
-    application.add_object(std::make_unique<app::Cube>(app::Point4d{100, 100, 201}, sf::Color::Magenta));
-    application.add_object(std::make_unique<app::Cube>(app::Point4d{100, 0, 201}, sf::Color::Cyan));
+    application.get_world().add_object(std::make_unique<app::Cube>(app::Point4d{0, 0, 50}, sf::Color::Black));
     application.run();
     return 0;
 }
